ddf4af68ed4e02b7b1f8a295186f22ed3abf4c24
Jaan6 <006teekjaan@gmail.com>
2024-07-31 13:35:29 +0530

    Initial commit

---
+++ src/app/dashboard/line-chart.tsx
@@ -0,0 +1,4 @@
+export default function LineChart(){
+    return  <h2>Line Chart</h2>
+       
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+function BarChart(){
+    return <h1> Bar Chart! </h1>
+
+}
+
+export default function DashBoard(){
+    return <h1> DashBoard!</h1>
+}
